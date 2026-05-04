ccb2a589dc534a5c079bb5d58e1d5285441f1737
Itadi02 <adityashaw053@gmail.com>
2026-05-04 13:45:45 +0530

    removed node_modules and added gitignore

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const { createServer } = require("node:http");
+const path = require("path");
+const app = express();
+const { Server } = require("socket.io");
+
+const server = createServer(app);
+const io = new Server(server);
+//app.use(express.static(path.join(__dirname, "public")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+io.on("connection", (socket) => {
+  console.log("A user is connected");
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+});
+server.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
